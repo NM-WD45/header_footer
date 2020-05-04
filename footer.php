c24c209b65f9baf4906f0037f6ef41e1a5cea519
N M <contact@nm-webdesign.fr>
2020-05-04 13:28:53 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer>
+  <p style="margin-bottom:50px;">
+  <a href="http://www.nm-webdesign.fr" target="_blank">
+    &copy; 2019 | <?php echo date('Y'); ?> monsite.fr 
+    <img src="http://www.noelle-monge.fr/Noelle_pose_1.png" height="60"> 
+    Réalisé par NM-WebDesign</a></p>
+    
+    <!-- Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </footer>
+
+</body>
+</html>
